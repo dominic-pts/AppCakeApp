4299ad06957f02e15fdcd3a019d03e28fe99b3d2
Pham Son <phamthanhson99999999@gmail.com>
2022-12-07 10:33:16 +0700

    xin chào đây là file push đầu tiên

---
+++ app/src/main/java/onekey/nhom10/myappcake/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package onekey.nhom10.myappcake.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import onekey.nhom10.myappcake.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
